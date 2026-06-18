239d277c529245d9e11149167f91be030c899f6f
dimsdevv <dmscmail0@gmail.com>
2026-06-18 22:19:00 +0700

    feat(analytics): integrate posthog analytics for visitor tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import './index.css'
 import App from './App.jsx'
 
+posthog.init(import.meta.env.VITE_POSTHOG_PROJECT_TOKEN, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <PostHogProvider client={posthog}>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </PostHogProvider>
   </StrictMode>,
 )
